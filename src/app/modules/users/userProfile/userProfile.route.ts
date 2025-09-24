0c4d787a417925c93b6ae55374a00c80592a832b
Abir-7 <md.tazwarul.islam.07@gmail.com>
2025-09-24 13:05:18 +0600

    update

--- src/app/modules/users/userProfile/userProfile.route.ts
+++ src/app/modules/users/userProfile/userProfile.route.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { auth } from "../../../middlewares/auth/auth";
 import { upload } from "../../../middlewares/fileUpload/fileUploadHandler";
 import { UserProfileController } from "./userProfile.controller";
+import { parseDataField } from "../../../middlewares/fileUpload/parseDataField";
 
 const router = Router();
 
 router.patch(
   "/update-profile",
+  upload.single("image"),
+  parseDataField("data"),
   auth("SUPERADMIN", "USER"),
   UserProfileController.updateProfile
 );
